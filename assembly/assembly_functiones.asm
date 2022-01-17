acc7374b7328ee8054b186f365dbe582b06b9dd2
gass-ita <97393534+gass-ita@users.noreply.github.com>
2022-01-17 15:12:24 +0100

    Create assembly_functiones.asm

---
+++ assembly/assembly_functiones.asm
@@ -0,0 +1,15 @@
+    ;double number print
+    mov al, molli
+    mov ah, 0
+    mov cl, 10
+    div cl
+    mov bh, ah
+    mov bl, al
+    add bh, 48
+    add bl, 48
+    mov ah, 02h
+    mov dl, bl
+    int 21h   
+    mov ah, 02h
+    mov dl, bh 
+    int 21h    

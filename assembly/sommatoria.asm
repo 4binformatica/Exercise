9f488695383398ece092835ea9616320aceaf185
gass-ita <97393534+gass-ita@users.noreply.github.com>
2022-01-17 11:04:57 +0100

    created sommatoria

--- assembly/sommatoria.asm
+++ assembly/sommatoria.asm
@@ -1,1 +1,60 @@
+; multi-segment executable file template.
 
+data segment
+    ; add your data here!
+    pkey db "press any key...$"
+ends
+
+stack segment
+    dw   128  dup(0)
+ends
+
+code segment
+start:
+; set segment registers:
+    mov ax, data
+    mov ds, ax
+    mov es, ax
+
+    mov ah, 01h
+    int 21h
+    sub al, 48
+    mov dl, al
+    mov bl, 0
+inizio_ciclo:
+    cmp dl, 0
+    jl fine
+    add bl, dl
+    dec dl
+    jmp inizio_ciclo
+
+fine:
+    mov al, bl
+    mov ah, 0
+    mov cl, 10
+    div cl
+    mov bh, ah
+    mov bl, al
+    add bh, 48
+    add bl, 48
+    mov ah, 02h
+    mov dl, bl
+    int 21h   
+    mov ah, 02h
+    mov dl, bh 
+    int 21h
+
+        
+    lea dx, pkey
+    mov ah, 9
+    int 21h        ; output string at ds:dx
+    
+    ; wait for any key....    
+    mov ah, 1
+    int 21h
+    
+    mov ax, 4c00h ; exit to operating system.
+    int 21h    
+ends
+
+end start ; set entry point and stop the assembler.

3af519a66b1d6c0459cdbb2c403df25a0354517f
Frederick67 <76555880+Frederick67@users.noreply.github.com>
2022-01-17 11:15:55 +0100

    I think I add this cycle because the world is beatyfull

---
+++ assembly/Countdown.asm
@@ -0,0 +1,55 @@
+; multi-segment executable file template.
+
+data segment
+    ; add your data here!
+    pkey db "press any key...$"
+    input db "Inserisci il numero--->$"
+ends
+
+stack segment
+    dw   128  dup(0)
+ends
+
+code segment
+start:
+; set segment registers:
+    mov ax, data
+    mov ds, ax
+    mov es, ax
+
+    ; add your code here
+
+
+    lea dx, input
+    mov ah, 9
+    int 21h   
+
+	mov ah, 1
+	int 21h
+    sub al, 48
+    mov cl, al
+
+    ;inizio ciclo
+ciclo:
+    mov dl,cl
+    add dl,48
+    mov ah,2
+    int 21h
+    dec cl
+    cmp cl, 0
+    jge ciclo
+
+
+    lea dx, pkey
+    mov ah, 9
+    int 21h        ; output string at ds:dx
+    
+    ; wait for any key....    
+    mov ah, 1
+    int 21h
+    
+    mov ax, 4c00h ; exit to operating system.
+    int 21h    
+ends
+
+end start ; set entry point and stop the assembler.
